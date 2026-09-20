5144fe98eeead5a63f6e4444b19780f958eac265
lihai4781-max <lihai4781@gmail.com>
2026-09-20 14:33:03 +0900

    Add files via upload

--- app/src/main/java/com/homestore/price/MainActivity.java
+++ app/src/main/java/com/homestore/price/MainActivity.java
@@ -13,6 +13,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
